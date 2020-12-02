e9dd363abd212cf54b20c0316620eb591397c0ba
chandulaed <50513412+chandulaed@users.noreply.github.com>
2020-12-02 06:14:34 +0530

    dashboard menu update

--- app/src/main/java/com/example/grocerylist/Dashbord.java
+++ app/src/main/java/com/example/grocerylist/Dashbord.java
@@ -4,9 +4,13 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toolbar;
 
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
@@ -14,34 +18,45 @@ import com.google.firebase.auth.FirebaseAuth;
 
 public class Dashbord extends AppCompatActivity {
 
-    TextView mail, name;
-    Button logout;
+    TextView mail;
+    androidx.appcompat.widget.Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashbord);
-
-        logout = findViewById(R.id.logout);
-        name = findViewById(R.id.name);
         mail = findViewById(R.id.mail);
-
+        toolbar = findViewById(R.id.home_tool_bar);
+        setSupportActionBar(toolbar);
         GoogleSignInAccount signInAccount = GoogleSignIn.getLastSignedInAccount(this);
-        if(signInAccount!=null){
-            name.setText(signInAccount.getDisplayName());
-            mail.setText(signInAccount.getEmail());
+        if (signInAccount != null) {
+            getSupportActionBar().setTitle("HELLO " + signInAccount.getGivenName());
+            mail.setText("UserID -- " + signInAccount.getEmail());
         }
-
-        logout.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                FirebaseAuth.getInstance().signOut();
-                Intent intent = new Intent (getApplicationContext(),Login.class);
-                startActivity(intent);
-            }
-
-        });
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_menu, menu);
+        return true;
+    }
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_add:
+                return true;
+            case R.id.logout:
+            {
+                FirebaseAuth.getInstance().signOut();
+                Intent intent = new Intent(getApplicationContext(), Login.class);
+                startActivity(intent);
+                return true;
+            }
+
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
 }
